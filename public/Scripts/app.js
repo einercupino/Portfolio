e56bc71bb2bb491aef436669f757720721dbd275
einercupino <einer.cupino@gmail.com>
2023-02-02 15:33:28 -0500

    first commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,11 @@
+// IIFE -- Immediately Invoked Function Expression
+
+(function(){
+
+    function Start() 
+    {
+        console.log("App started..");
+    }
+
+    window.addEventListener("load",Start);
+})();
---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+/* HOME */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home'});
+});
+
+/* HOME */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home'});
+});
+
+/* ABOUT US */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me'});
+});
+
+/* PRODUCTS */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects'});
+});
+
+/* SERVICES */
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services'});
+});
+
+
+/* CONTACT US */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Us'});
+});
+
+module.exports = router;
