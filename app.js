344b5a7f67a5ec2512dc283158d0ce48b923472d
Einer Cupino <ecupino@my.centennialcollege.ca>
2024-03-31 00:59:53 -0400

    updated

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ let usersRouter = require('./routes/users');
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 
 app.use(logger('dev'));
 app.use(express.json());
